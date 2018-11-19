5acbcd7bed14bbac0986f11d5686c0930ee92cf8
MelvinSalcedo <melvinsalcedo169@gmail.com>
2018-11-18 21:35:33 -0500

    pruebas

---
+++ pruebas/CalculatorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+require 'Calculator.php';
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTests extends TestCase
+{
+    private $calculator;
+ 
+    protected function setUp()
+    {
+        $this->calculator = new Calculator();
+    }
+ 
+    protected function tearDown()
+    {
+        $this->calculator = NULL;
+    }
+ 
+    public function testAdd()
+    {
+        $result = $this->calculator->add(4, 2);
+        $this->assertEquals(3, $result);
+    }
+ 
+}
